8b56e010223b5789d2edd779cfc6f1778f24c5c0
Desmond-netw <dsquare200@gmail.com>
2024-11-22 19:46:50 +0200

    Added a script to preview imge

---
+++ preview_image/script.js
@@ -0,0 +1,23 @@
+function previewImage(event) {
+    const showImage = document.getElementById('previewContainer');
+    const file = event.target.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            img.style.width = '100%';
+            img.style.height = '100%';
+            img.style.objectFit = 'contain';
+            showImage.innerHTML = '';
+            showImage.appendChild(img);
+        };
+
+        reader.readAsDataURL(file);
+    } else {
+        //keep container empty
+        showImage.innerHTML = 'No image selected';
+    }
+}
